9669e66bad1738065432c32d0809da02500c24ae
Yashas-Kantharaj <yashastkd@gmail.com>
2021-04-15 15:49:32 +0530

    filters any all none

--- src/main/kotlin/collections/filters.kt
+++ src/main/kotlin/collections/filters.kt
@@ -8,5 +8,9 @@ fun main() {
     println(nos)
     val postives=nos.filter { it > 0}
     println("Postive nos: $postives")
-
+    val doubles=nos.map{ x -> x*x}
+    println("Doubles nos: $doubles")
+    println("Any nos greater than 3: "+ nos.any{it>2})
+    println("All nos greater than 4: "+ nos.all{it <4})
+    println("none nos greater than 5: "+ nos.none{it > 5})
 }
